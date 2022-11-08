3ebd067e76d98f0fa68884950905617fcf5ad80d
戴海洋 <haiyang.dai@davincimotor.com>
2022-11-08 15:02:06 +0800

    pref: 代码

--- src/index.ts
+++ src/index.ts
@@ -26,22 +26,16 @@ export default function(source: string) {
   // 保存所有顶层的声明
   const declarations = new Map()
 
-  const isInsert = isSaveDeclaration(ast, importPath, declarations)
-
   // 如果插入过就返回
-  if (isInsert) return source
+  if (isSaveDeclaration(ast, importPath, declarations)) return source
 
   const { importedDeclaration, importedComponent } = startInsert(ast, componentName, importPath, declarations)
 
-  if (!importedComponent && logError) {
-    webpackEnv.emitWarning(
-      `页面: ${codeFilePath} 注入组件失败，建议手动引入组件。组件注入限制请查阅: https://github.com/xdoer/taro-inject-component-loader`,
-    )
-  }
+  if ((!importedComponent || !importedDeclaration) && logError) {
+    const tip = (!importedComponent && '组件') || !importedDeclaration || '导入声明'
 
-  if (!importedDeclaration && logError) {
     webpackEnv.emitWarning(
-      `页面: ${codeFilePath} 注入导入声明失败，建议手动引入组件。组件注入限制请查阅: https://github.com/xdoer/taro-inject-component-loader`,
+      `页面: ${codeFilePath} 注入${tip}失败，建议手动引入组件。组件注入限制请查阅: https://github.com/webpack-all-project/taro-inject-components-loader`,
     )
   }
 
